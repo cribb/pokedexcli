e05de3b770a5be150c0f748b5c069c90925f4238
Jeremy Cribb <cribb.jeremy@gmail.com>
2026-07-05 14:14:44 -0400

    Added basic REPL prompt. For now it only returns the zeroth word 'command' from stdin. Also corrected ignore file.

--- repl.go
+++ repl.go
@@ -1,6 +1,28 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func goREPL() {
+	// fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// fmt.Println("Foobar, bitches!")
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+		text := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %v\n", text[0])
+		// fmt.Printf("repl_input: %v", repl_input)
+	}
+}
 
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(text))
