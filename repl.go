4fd55ed0cfc4a0f363c94436e2c2b9d09ffb5a4d
Jeremy Cribb <cribb.jeremy@gmail.com>
2026-07-06 11:06:03 -0400

    implemented help and exit commands.

--- repl.go
+++ repl.go
@@ -7,9 +7,17 @@ import (
 	"strings"
 )
 
+// Type definitions occur at "package level" (global scope)
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func goREPL() {
-	// fmt.Println("Hello, World!")
+
 	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
 
 	// fmt.Println("Foobar, bitches!")
 	for {
@@ -18,10 +26,57 @@ func goREPL() {
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading standard input:", err)
 		}
-		text := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %v\n", text[0])
-		// fmt.Printf("repl_input: %v", repl_input)
+		commandLine := cleanInput(scanner.Text())
+		commandName := commandLine[0]
+
+		command, ok := commands[commandName]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := command.callback()
+
+		if err != nil {
+			fmt.Printf("Error '%v' occurred.\n", err)
+		}
+
 	}
+
+	// return nil
+}
+
+func getCommands() map[string]cliCommand {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+	return commands
+}
+
+func commandHelp() error {
+	fmt.Printf("\nWelcome to the Pokedex!\nUsage:\n\n")
+	// for INDEX, ELEMENT := range SLICE {}
+	commands := getCommands()
+	for _, command := range commands {
+		fmt.Printf("%v\t%v\n", command.name, command.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
 
 func cleanInput(text string) []string {
